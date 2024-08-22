b83ac6b297f053e27b9d4f4ab2064b31785c9078
Anderson Jimenez <andersonjesusujv@hotmail.com>
2024-08-22 11:10:20 -0500

    custom button creado

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -78,8 +78,34 @@ class _ButtonsView extends StatelessWidget {
                 backgroundColor: WidgetStatePropertyAll(colors.primary)
               ),
             ),
+
+            const CustomButton()
     
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: (){},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Custom', style: TextStyle(color: Colors.white),),
+          ),
         ),
       ),
     );
